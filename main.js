8b71080b831872ca4b64f2e729da9dbfbeae51a0
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-07-10 14:09:04 +0700

    Update

---
+++ main.js
@@ -0,0 +1,58 @@
+const SHA256 = require('crypto-js/sha256');
+class Block{
+    constructor(index, timestamp, data, previousHash=''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+    calculateHash(){
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+    createGenesisBlock(){
+        return new Block(0, "01/06/2021", "Genesis Block", "0");
+    }
+    getLatestBlock(){
+        return this.chain[this.chain.length-1];
+    }
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+    isChainValid(){
+        for(let i=1; i<this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let fredio = new Blockchain();
+fredio.addBlock(new Block(1, "02/06/2021", {amount: 1000}));
+fredio.addBlock(new Block(2, "03/06/2021", {amount: 2000}));
+
+console.log(JSON.stringify(fredio, null, 4));
+
+console.log("Status Block " + fredio.isChainValid());
+
+fredio.chain[1].data = {amount : 100};
+
+console.log("Status Block " + fredio.isChainValid());
+
+
+
